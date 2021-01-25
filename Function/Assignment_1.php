f1dd8da3ee85a7dbc2d5054997a5f8a56fe9ca77
Mazher <hossainmazher@gmail.com>
2021-01-25 18:54:19 +0600

    Function Assignment 1 done

--- Function/Assignment_1.php
+++ Function/Assignment_1.php
@@ -3,9 +3,35 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+
+
+  <title>Assignment 1</title>
+  <!--এমন একটি ফাংসন বানান যাকে কি না একটা বয়স দিলে একজন কি বৃদ্ধ, যুবক, কিশো, ছেলে , বাচ্ছা রিটান করবে-->
+
+
 </head>
 <body>
-  
+  <?php
+
+    function age($age){
+      if($age <= 11){
+        echo "This person is a child who is $age year's old.";
+      }
+      elseif($age >11 and $age<= 20){
+        echo "This person is a young human who is $age year's old.";
+      }
+      elseif($age >20 AND $age<= 60){
+        echo "This human is an adult who is $age year's old.";
+      }
+      elseif($age > 60){
+        echo "This is an old person who is $age year's old";
+      }
+      else{
+        echo "Invalid number.";
+      }
+    }
+
+    age(21);
+  ?>
 </body>
 </html>
