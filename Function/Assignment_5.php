5d473c103c93d3492f8d6f4a462e87d43372013f
Mazher <hossainmazher@gmail.com>
2021-01-25 20:16:44 +0600

    ignore removed

---
+++ Function/Assignment_5.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  
+  <title>Assignment 5</title>
+  <!--একটি হেডিং ফাংসন বানান যেটা দিয়ে ডাইনামিস হেডিং তৈরি করা যাবে-->
+
+
+</head>
+<body>
+  <?php
+    function heading(){
+      
+    }
+    heading();
+  ?>
+</body>
+</html>
