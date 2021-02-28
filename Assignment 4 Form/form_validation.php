6715936faf197e5a010d8422958feeb5f7ed7c50
Mazher <hossainmazher@gmail.com>
2021-02-28 18:31:18 +0600

    Form validation Assignment started

---
+++ Assignment 4 Form/form_validation.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+  <title>Form Validation</title>
+  <!--
+    #Form Value Get
+    #Form Validation Global
+    #Form Field Validation
+    #Email Validation
+    #Specific Email Validation
+    #Phone number validation
+  -->
+
+
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/fonts/css/all.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+    <div class="wrap shadow box">
+      <div class="card">
+        <div class="card-body bd">
+          <h2>Sign in</h2>
+          <form action="">
+            <div class="form-group">
+              <label for="">Name</label>
+              <input class="form-control" type="text">
+            </div>
+            <div class="form-group">
+              <label for="">Email</label>
+              <input class="form-control" type="text">
+            </div>
+            <div class="form-group">
+              <label for="">Cell</label>
+              <input class="form-control" type="text">
+            </div>
+            <div class="form-group">
+              <label for="">Age</label>
+              <input class="form-control" type="text">
+            </div>
+            <div class="form-group mt-5">
+              <input class="btn btn-primary" type="submit" value="Sign In">
+            </div>
+        </div>
+      </div>
+    </div>
+  
+  <script src="assets/js/jquery-3.5.1.min.js"></script>
+  <script src="assets/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+  <script src="assets/js/script.js"></script>
+</body>
+</html>
