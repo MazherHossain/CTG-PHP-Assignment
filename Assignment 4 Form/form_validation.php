182c92f7aa7db4c837992ec65fa4af7f0fb01b87
Mazher <hossainmazher@gmail.com>
2021-03-01 21:24:18 +0600

    age and phone number validated

--- Assignment 4 Form/form_validation.php
+++ Assignment 4 Form/form_validation.php
@@ -29,36 +29,49 @@
       $cell = $_POST['cell'];
       $age = $_POST['age'];
 
-      $email_arr = explode('@', $email);//email exploding
-      $spec_mail = end($email_arr);
+      if(isset($email)){
+        $email_arr = explode('@', $email);//email exploding
+        $spec_mail = end($email_arr);
+      }
+
+      $cell_start = substr($cell, 0, 3);//cell manage
 
       if(empty($name)){
-        $err['name'] = "<p style='color:red;'> *required </p>";
+        $err['name'] = "<p style='color:red;'> *required </p>";//warning for not entering the value
       }
       if(empty($email)){
-        $err['email'] = "<p style='color:red;'> *required </p>";
+        $err['email'] = "<p style='color:red;'> *required </p>";//warning for not entering the value
       }
       if(empty($cell)){
-        $err['cell'] = "<p style='color:red;'> *required </p>";
+        $err['cell'] = "<p style='color:red;'> *required </p>";//warning for not entering the value
       }
       if(empty($age)){
-        $err['age'] = "<p style='color:red;'> *required </p>";
+        $err['age'] = "<p style='color:red;'> *required </p>";//warning for not entering the value
       }
 
     /**Form input validation for all input**/
       if( empty($name) || empty($email) || empty($cell) || empty($age) ){
 
-        $msg = " <p class='alert alert-danger alert-dismissible fade show' role='alert'>All information are required! <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close' ></button> </p>";
+        $msg = " <p class='alert alert-danger alert-dismissible fade show' role='alert'>All information are required! <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close' ></button> </p>";//warning for not entering any or all information
 
       }
       else if(filter_var($email, FILTER_VALIDATE_EMAIL) == false){
-        $msg = " <p class='alert alert-warning alert-dismissible fade show' role='alert'>Invalid Email Address! <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close' ></button> </p>";
+        $msg = " <p class='alert alert-warning alert-dismissible fade show' role='alert'>Invalid Email Address! <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close' ></button> </p>";//Email validation warning
+      }
+      else if($cell>8801999999999 || $cell<01300000000){
+        $msg = " <p class='alert alert-danger alert-dismissible fade show' role='alert'>Invalid phone number input data type! <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close' ></button> </p>";//warning for not entering integer value as phone number
+      }
+      else if(in_array($cell_start, ['017','018','019','015','013','016','014'])==false){
+        $msg = " <p class='alert alert-warning alert-dismissible fade show' role='alert'>Invalid phone number! <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close' ></button> </p>";//Phone number validation warning
       }
       else if($spec_mail != 'yahoo.com'){
-        $msg = " <p class='alert alert-warning alert-dismissible fade show' role='alert'>The Email Address must be of Yahoo! <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close' ></button> </p>";
+        $msg = " <p class='alert alert-warning alert-dismissible fade show' role='alert'>The Email Address must be of Yahoo! <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close' ></button> </p>";//Specific Email address validation
+      }
+      else if($age < 18 && $age > 39){
+        $msg = " <p class='alert alert-warning alert-dismissible fade show' role='alert'>Only people aged 18 to 39 are allowed to sign in! <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close' ></button> </p>";//Specific age category validation
       }
       else
-        $msg = "<p class='alert alert-success alert-dismissible fade show' role='alert'>You're good to go! <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close' ></button> </p>";
+        $msg = "<p class='alert alert-success alert-dismissible fade show' role='alert'>You're good to go! <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close' ></button> </p>";//Success validation message
       }
   ?>
 
