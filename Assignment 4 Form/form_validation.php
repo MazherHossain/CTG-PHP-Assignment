0a8474e54729d2cbd4119b15cb13cc8c923fe15d
Mazher <hossainmazher@gmail.com>
2021-03-02 05:37:36 +0600

    commented out

--- Assignment 4 Form/form_validation.php
+++ Assignment 4 Form/form_validation.php
@@ -134,6 +134,6 @@
   <script src="assets/js/jquery-3.5.1.min.js"></script>
   <script src="assets/js/bootstrap.bundle.min.js"></script>
   <script src="assets/js/bootstrap.min.js"></script>
-  <script src="assets/js/script.js"></script>
+  <!--<script src="assets/js/script.js"></script>-->
 </body>
 </html>
